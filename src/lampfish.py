bc472244cd51e3439cf8a0792bb24acd58d36256
Brian Hardenstein <bhardens@yahoo.com>
2018-02-14 05:06:30 -0800

    update two using two scripts, which use pika to send messages on a queue between the two processes

--- src/lampfish.py
+++ src/lampfish.py
@@ -1,103 +1,28 @@
 #!/usr/bin/python
 # Author: Brian Hardenstein
 
-# Simple strand test for Adafruit Dot Star RGB LED strip.
-# This is a basic diagnostic tool, NOT a graphics demo...helps confirm
-# correct wiring and tests each pixel's ability to display red, green
-# and blue and to forward data down the line.  By limiting the number
-# and color of LEDs, it's reasonably safe to power a couple meters off
-# USB.  DON'T try that with other code!
-
-import time
-import RPi.GPIO as GPIO
-from dotstar import Adafruit_DotStar
 from flask import Flask, render_template, request
-import multiprocessing
-import psutil
+import pika
 app = Flask(__name__)
 
-# Alternate ways of declaring strip:
-#  Adafruit_DotStar(npix, dat, clk, 1000000) # Bitbang @ ~1 MHz
-#  Adafruit_DotStar(npix)                    # Use SPI (pins 10=MOSI, 11=SCLK)
-#  Adafruit_DotStar(npix, 32000000)          # SPI @ ~32 MHz
-#  Adafruit_DotStar()                        # SPI, No pixel buffer
-#  Adafruit_DotStar(32000000)                # 32 MHz SPI, no pixel buf
-# See image-pov.py for explanation of no-pixel-buffer use.
-# Append "order='gbr'" to declaration for proper colors w/older DotStar strips)
 
+def send_to_lights(data):
+    ''' data is a string that will be parsed by the receiving end and turned
+        into light string commands.  String should be in the format of:
+        mode_brightness
+        ex: 'on_255' OR 'on_128' OR 'off_0'
+    '''
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 
-def run_strip(mode="off", brightness=255):
-    numpixels = 240     # Number of LEDs in strip
+    channel = connection.channel()
 
-    print("run_strip inbound mode:", mode)
-    # Here's how to control the strip from any two GPIO pins:
-    # datapin = 10
-    # clockpin = 11
-    strip = Adafruit_DotStar(numpixels, 12000000)
+    channel.queue_declare(queue="lampfish")
 
-    # brightness is an integer from 1 to 255
-    # mode, in time, should enable other things besides just being full on
-
-    strip.begin()                       # Initialize pins for output
-    strip.setBrightness(brightness)     # Limit brightness to ~1/4 duty cycle
-
-    # Runs 10 LEDs at a time along strip, cycling through red, green and blue.
-    # This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
-
-    # used for mode: comet
-    # head = 0               # Index of first 'on' pixel
-    # tail = -10             # Index of last 'off' pixel
-    color = 0xFFFFFF        # 'On' color (starts red)
-
-    if mode == "off":
-        print("run_strip: turn off strip")
-        GPIO.cleanup()
-        strip.clear()
-        strip.show()
-        print("done")
-
-    elif mode == "on":
-        print("run_strip: starting strip")
-        try:
-            while True:                              # Loop forever
-
-                for idx in range(numpixels):
-                    strip.setPixelColor(idx, color)
-
-                # strip.setPixelColor(head, color) # Turn on 'head' pixel
-                # strip.setPixelColor(tail, 0)     # Turn off 'tail'
-                strip.show()                     # Refresh strip
-                time.sleep(1.0 / 50)             # Pause 20 milliseconds (~50 fps)
-
-                # head += 1                        # Advance head position
-                # if(head >= numpixels):           # Off end of strip?
-                #    head    = 0              # Reset to start
-                #    color >>= 8              # Red->green->blue->black
-                #    if(color == 0): color = 0xFF0000 # If black, reset to red
-
-                # tail += 1                        # Advance tail position
-                # if(tail >= numpixels): tail = 0  # Off end? Reset
-
-        except KeyboardInterrupt:
-            print "cleaning up"
-            GPIO.cleanup()
-            strip.clear()
-            strip.show()
-            print "done"
-
-        except SystemError as err:
-            print("SystemError:", err)
-
-
-def task_runner(mode, brightness=255):
-    processes = psutil.Process().children()
-    for process in processes:
-        process.kill()
-
-    print("task_runner -> var", mode)
-
-    process = multiprocessing.Process(target=run_strip, args=(mode, brightness,))
-    process.start()
+    channel.basic_publish(exchange="",
+                          routing_key="lampfish",
+                          body=data)
+    print("send_to_rabbitmq -> sent:", data)
+    connection.close()
 
 
 @app.route("/")
@@ -120,21 +45,17 @@ def main():
 @app.route("/<action>")
 def action(action):
 
-    print("action", action)
-    task_runner(mode=action)
     # run_strip(mode=action)
-    # # If the action part of the URL is "on," execute the code indented below:
-    # if action == "on":
-    #     # Set the pin high:
-    #     run_strip(mode="on")
-    #     lamp_on = True
-    #     # Save the status message to be passed into the template:
-    #     message = "Turned lamp on."
-    #
-    # if action == "off":
-    #     run_strip(mode="off")
-    #     lamp_on = False
-    #     message = "Turned lamp off."
+    # If the action part of the URL is "on," execute the code indented below:
+    if action == "on":
+        # Set the pin high:
+        send_to_lights("on_255")
+        # Save the status message to be passed into the template:
+        print("action -> Sent 'lamp on' to queue")
+
+    if action == "off":
+        send_to_lights("on_255")
+        print("action -> Sent 'lamp off' to queue")
 
     # print("message: ", message)
 
